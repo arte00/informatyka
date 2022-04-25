cddb81d29547499ddc34b4c347887b47365e382e
arte00 <hubi.lukki@gmail.com>
2022-04-25 23:29:26 +0200

    quad tree encoding

--- SystemyMultimedialne/Lab06/main.py
+++ SystemyMultimedialne/Lab06/main.py
@@ -1,7 +1,11 @@
+from platform import node
 import sys
+from cv2 import bitwise_and
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import scipy as sp
+from tqdm import tqdm
 
 def load_image(path, infilename) :
     img = Image.open(path + infilename)
@@ -55,18 +59,21 @@ def encode_rle(_data):
     # pair = bit and how many times bit occured in row [1, 1, 4, 5, 5, 5] = [2, 1, 1, 4, 3, 5]
     pair_index = dimension + 1
     bit_index = 0
-    while bit_index < data_flatten.shape[0]:
-        current_bit = data_flatten[bit_index]
-        repeats = 1
-        while bit_index + repeats < data_flatten.shape[0] and current_bit == data_flatten[bit_index + repeats]:
-            repeats += 1
+    with tqdm(total=data_flatten.shape[0]) as pbar:
+        while bit_index < data_flatten.shape[0]:
+            current_bit = data_flatten[bit_index]
+            repeats = 1
+            while bit_index + repeats < data_flatten.shape[0] and current_bit == data_flatten[bit_index + repeats]:
+                repeats += 1
 
-        bit_index += repeats
-        encoded_data[pair_index] = repeats
-        encoded_data[pair_index + 1] = current_bit
-        pair_index += 2
+            bit_index += repeats
+            pbar.update(bit_index)
+            encoded_data[pair_index] = repeats
+            encoded_data[pair_index + 1] = current_bit
+            pair_index += 2
 
     return encoded_data[:pair_index]
+    
 
 def decore_rle(_data):
     
@@ -96,8 +103,41 @@ def decore_rle(_data):
 
     return decoded_data
 
+'''QUAD TREE'''
 
-if __name__ == '__main__':
+node_counter = 0
+
+def encode_quad(data, max=-1, level=0):
+    global node_counter
+    node_counter += 1
+
+    color = color = np.mean(data, axis=(0, 1)).astype(data.dtype)
+
+    quad_top_left = None
+    quad_top_right = None
+    quad_bot_right = None
+    quad_bot_left = None
+
+    if not (data == color).all() and level != max and data.shape[0] > 1 and data.shape[1] > 1:
+        split_width = np.array_split(data, 2, axis=1)
+        top_left, bot_left = np.array_split(split_width[0], 2, axis=0)
+        top_right, bot_right = np.array_split(split_width[1], 2, axis=0)
+        quad_top_left = encode_quad(top_left, max, level + 1)
+        quad_top_right = encode_quad(top_right, max, level + 1)
+        quad_bot_right = encode_quad(bot_right, max, level + 1)
+        quad_bot_left = encode_quad(bot_left, max, level + 1)
+
+    return (color, level, quad_top_left, quad_top_right, quad_bot_right, quad_bot_left)
+
+def decode_quad(node):
+    pass
+
+def join(top_left, top_right, bot_right, bot_left):
+    top = np.concatenate(top_left, top_right, axis=1)
+    bot = np.concatenate(bot_right, bot_left, axis=1)
+    return np.concatenate(top, bot, axis=0)
+
+def rle_testing():
     path = 'Lab06/'
     image = load_image(path, 'rysunek_techniczny.jpg')
     size = get_size(image)
@@ -112,3 +152,22 @@ if __name__ == '__main__':
     
     plt.imshow(decoded, cmap='gray', vmin=0, vmax=255)
     plt.show()
+
+
+def quad_testing():
+
+    # image = np.dstack([np.eye(7),np.eye(7),np.eye(7)])
+    path = 'Lab06/'
+    # _image = load_image(path, 'monkey.jpg')
+    _image = np.eye(8)
+    image = _image.copy()
+    print("image", get_size(image))
+    print(image)
+    splitted = encode_quad(image, 1)
+    print(splitted[2])
+    print("splitted", get_size(splitted))
+    print("node_counter", node_counter)
+
+
+if __name__ == '__main__':
+    quad_testing()
